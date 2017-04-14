367fe003c8f94b6060fe31f1ad81294792dd0fe1
Stefano Bucci <stilefano@gmail.com>
2017-04-14 02:21:16 +0200

    working on create detail data

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.udacity.stockhawk.ui;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
 
 import timber.log.Timber;
 
@@ -18,6 +21,24 @@ public class DetailActivity extends AppCompatActivity {
 
         LineChart chart = (LineChart) findViewById(R.id.chart);
 
+        Intent intent = getIntent();
+        String symbol = intent.getStringExtra("Symbol");
+
+        String history = getHistoryString(symbol);
+
+    }
+
+    private String getHistoryString(String symbol) {
+        Cursor cursor = getContentResolver().query(Contract.Quote.makeUriForStock(symbol), null, null, null, null);
+
+        String history = "";
+        if (cursor.moveToFirst()) {
+            history = cursor.getString(cursor.getColumnIndex(Contract.Quote.COLUMN_HISTORY));
+            cursor.close();
+        }
+
+
+        return history;
     }
 
 }
